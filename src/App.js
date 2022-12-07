728c82e57b31b1bafd3114faf09ef65ed79c0035
salomedolidze <dolidzesalome36@gmail.com>
2022-12-07 23:41:00 +0400

    reacthomework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const generateUsers = () => {
+  let users = [ "user1", "user2", " user3", " user4", " user5", " user6", " user7", " user8", " user9", " user10",
+  ];
+
+  // let item = users[Math.floor(Math.random()*users.length)];
+  // console.log(item)
+  return users;
+
+};
+
 
 function App() {
+  const [names, setNames] = useState([]);
+  useEffect(() => {
+    setNames(generateUsers());
+  }, []);
+  useEffect(()=>{ 
+       document.title = names.length + "users left"
+},[names]
+  )
+
+  console.log(names);
+
+
+  const removeRendomList = () => {
+    const randomlist = names[Math.floor(Math.random() * names.length)];
+    setNames(names.filter((element) =>{
+      return  element !== randomlist
+    }));
+  };
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+    {names.map((elem, index) => {
+        return <li key={index}>{elem}</li>;
+      })}
+    </ul>
+    <button onClick={removeRendomList}> click here</button>
+     
+    </>
   );
 }
 
